12263d7c2eeff9d995a71a47e8f850ab6f054ecb
Termi11 <91840662+Termi11@users.noreply.github.com>
2021-11-14 23:08:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,12 @@ using namespace std;
 int main() {
    int n=0, x1=0, x2=1, xf=x1+x2;
     cin >> n;
-    cout << x2 << endl;
-
-    for(int i=2; i<(n+1);i++){
-       if (c<=n){
+    cout << "1" << endl;
+    for(int i=0; i<=(n-1);i++){
             cout << xf << endl;
             x1=x2;
             x2=xf;
             xf=x1+x2;
        }
-    }
    return 0;
 }
